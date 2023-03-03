ae42b60de355c46c1228e1659382fb2bffd7011a
nikhil naroliya <95118417+NikhilNaroliya@users.noreply.github.com>
2023-03-03 22:57:31 +0530

    Update index.js
    added FP set 2 problematic question

--- index.js
+++ index.js
@@ -367,3 +367,23 @@
 // const filteredEmployees = employees.filter(filterBySalary)
 // console.log(filteredEmployees);
 // // Output: [{name: "Joice", age: 30, salary: 35000}, {name: "Jeena", age: 40, salary: 50000}]
+
+// Write an ES6 function that takes an array of objects containing Bollywood movie information 
+// (title, director, year, rating) and returns an array with only the movies that were made before 1990 
+// and has a rating above 8.0. (Question Level: tough)
+
+//  const BestMovies=(movie)=> movie.year<1990 && movie.rating>8.0
+
+// const filtered=(mov)=>mov.title
+    
+// const bollywoodMovies = [
+//   { title: 'Sholay', director: 'Ramesh Sippy', year: 1975, rating: 8.2 },
+//   { title: 'Amar Akbar Anthony', director: 'Manmohan Desai', year: 1977, rating: 7.6 },
+//   { title: 'Namak Halaal', director: 'Prakash Mehra', year: 1982, rating: 7.4 },
+//   { title: 'Mr. India', director: 'Shekhar Kapur', year: 1987, rating: 7.8 },
+//   { title: 'Qayamat Se Qayamat Tak', director: 'Mansoor Khan', year: 1988, rating: 7.6 },
+//   { title: 'Parinda', director: 'Vidhu Vinod Chopra', year: 1989, rating: 8.1 },
+//   { title: 'Dil', director: 'Indra Kumar', year: 1990, rating: 7.8 }
+// ];
+
+// console.log(bollywoodMovies.filter(BestMovies).map(filtered))
